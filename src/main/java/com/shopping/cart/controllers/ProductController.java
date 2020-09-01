44f54f95d2cd02fcbfa59ff5cc7c8dbf4e39468a
Shivomkara Chaturvedi <shivomkara.chaturvedi@soprasteria.com>
2020-09-01 16:34:12 +0530

    Update ProductController.java
    get product by id service implemented

--- src/main/java/com/shopping/cart/controllers/ProductController.java
+++ src/main/java/com/shopping/cart/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +52,12 @@ public class ProductController {
 			@RequestParam(value = "searchText", defaultValue = "") String searchText){
 		Page<Product> products= productService.findByProductName(searchText, pageNo, pageSize, sortBy);
 		return ResponseEntity.ok().body(products);
+	}
+	
+	@GetMapping("/getbyid/{id}")
+	public ResponseEntity<?> findAll(@PathVariable("id") Integer id){
+		Product product= productService.getById(id);
+		return ResponseEntity.ok().body(product);
 	}
 	
 	@PostMapping("/save")
